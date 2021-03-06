2a2a45c96ed3445d1bb7278a9d7ea2758f901f08
joshross <josh.ross1234@gmail.com>
2021-03-06 10:42:47 -0500

    Hello world run cmd implementation done

--- src/commands/run.js
+++ src/commands/run.js
@@ -1,22 +1,19 @@
-const {Command, flags} = require('@oclif/command')
+const {Command} = require('@oclif/command')
 
 class RunCommand extends Command {
   async run() {
-    const {flags} = this.parse(RunCommand)
-    const name = flags.name || 'world'
-    this.log(
-      `hello ${name} from /Users/joshross/Documents/Github/js-cli-tool/src/commands/run.js`
-    )
+    const {args} = this.parse(RunCommand)
+    const file = args.file
+    this.log(`js file path is: ${file}`)
   }
 }
 
-RunCommand.description = `Describe the command here
-...
-Extra documentation goes here
-`
+RunCommand.description = 'Run a JS file in a secure isolated process or thread'
 
-RunCommand.flags = {
-  name: flags.string({char: 'n', description: 'name to print'}),
-}
+RunCommand.args = [{
+  name: 'file',
+  description: 'path to js file to run',
+  required: true,
+}]
 
 module.exports = RunCommand
